7a5060cec93fe5da086ea2797e2d9d725d294171
Tobi-Stani <stanislavskytobias@gmail.com>
2024-09-23 11:15:27 -0300

    agregado ejercicio

--- ejerciciosGuia/claseNota.py
+++ ejerciciosGuia/claseNota.py
@@ -14,3 +14,12 @@ class Nota:
     
     def desaprobado(self):
         return self._valor < 6
+
+    def recuperar(self, nuevoValor):
+        if 0 <= nuevoValor <= 10:
+            if nuevoValor > self._valor:
+                self._valor = nuevoValor
+            else: 
+                print(f"El nuevo valor {nuevoValor} no es superior al acutal : {self._valor}. La nota no se cambia")
+        else:
+            print("el valor de la nota debe estar entre 0 y 10.")
