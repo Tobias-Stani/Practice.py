8c63ce8a3940adb803e5452b42825bc2e8a4da83
Tobi-Stani <stanislavskytobias@gmail.com>
2024-10-14 16:06:30 -0300

    agregado ejercicio 5

---
+++ ejerciciosGuia/Polimorfismo/ej5.py
@@ -0,0 +1,19 @@
+class Vehiculo:
+    pass
+
+class VehiculoTerrestre(Vehiculo):
+    pass
+
+class VehiculoAreo(Vehiculo):
+    pass
+
+class VehiculoAcuatico(Vehiculo):
+    pass
+
+
+#clases hijas de vehiculoTerrestre
+class VehiculoCarretera(VehiculoTerrestre):
+    pass
+
+class VehiculoVias(VehiculoTerrestre):
+    pass
