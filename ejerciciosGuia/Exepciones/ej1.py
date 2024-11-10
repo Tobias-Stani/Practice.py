e4935809baffabd8adda15b22a13e51bc38b2fda
Tobi-Stani <stanislavskytobias@gmail.com>
2024-11-09 23:33:29 -0300

    mejorado codigo

--- ejerciciosGuia/Exepciones/ej1.py
+++ ejerciciosGuia/Exepciones/ej1.py
@@ -4,10 +4,8 @@ class MiClase:
             # Generamos una excepción con un mensaje
             raise Exception("Este es un mensaje de error personalizado.")
         except Exception as e:
-            # Capturamos la excepción e imprimimos el mensaje
             print(f"Se capturó una excepción: {e}")
         finally:
-            # Este bloque se ejecuta siempre, haya o no una excepción
             print("Esto se imprime sin importar si hubo una excepción o no.")
 
 # Crear un objeto de la clase y ejecutar el método main
